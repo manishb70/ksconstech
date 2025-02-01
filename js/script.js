ada56fb0fa3a6730c856ee2f3e27de801d32bec7
Manish <mb8882568627@gmail.com>
2025-02-01 23:56:02 +0530

    update success

---
+++ js/script.js
@@ -0,0 +1,25 @@
+console.log("grgrg")
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  var userName = document.getElementById('user_name').value;
+  var userEmail = document.getElementById('user_email').value;
+  var message = document.getElementById('user_message').value;
+
+  // Replace the Service ID, Template ID, and User ID with your own credentials
+  emailjs.send("service_cawqdft","template_cesdga7",{
+    from_name: userName,
+    message: message,
+    sernder_email: userEmail,
+    reply_to: userEmail,
+    to_name: userName,
+    replly_to: userEmail,
+    })
+  .then(function(response) {
+    alert('Message sent successfully!', response);
+  }, function(error) {
+    alert('Failed to send message. Please try again.', error);
+  });
+});
+
