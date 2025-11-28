0c2b64a26dadb96bd21e9918b9e8abecdffb3a19
Manish <mb8882568627u@gmail.com>
2025-11-28 23:07:02 +0530

    New updaet

--- js/script.js
+++ js/script.js
@@ -8,20 +8,22 @@ document.getElementById('contact-form').addEventListener('submit', function(even
   var message = document.getElementById('user_message').value;
 
   // Replace the Service ID, Template ID, and User ID with your own credentials
-  emailjs.send("service_cawqdft","template_cesdga7",{
-    from_name: userName,
-    message: message,
-    sernder_email: userEmail,
-    reply_to: userEmail,
-    to_name: userName,
-    replly_to: userEmail,
-    })
-  .then(function(response) {
-    alert('Message sent successfully!', response);
-    document.getElementById('contact-form')
-    document.getElementById('contact-form').reset();
-  }, function(error) {
-    alert('Failed to send message. Please try again.', error);
-  });
+  // emailjs.send("service_cawqdft","template_cesdga7",{
+  //   from_name: userName,
+  //   message: message,
+  //   sernder_email: userEmail,
+  //   reply_to: userEmail,
+  //   to_name: userName,
+  //   replly_to: userEmail,
+  //   })
+  // .then(function(response) {
+  //   alert('Message sent successfully!', response);
+  //   document.getElementById('contact-form')
+  //   document.getElementById('contact-form').reset();
+  // }, function(error) {
+  //   alert('Failed to send message. Please try again.', error);
+  // });
+
+  alert("Message send successfully ")
 });
 
